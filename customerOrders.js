c07767bb4442ed5c0a6aee4c3c15ddcfc69e1f93
Richard Olpindo <djricky088@yahoo.com>
2024-10-03 23:26:11 -1000

    Created top 5 spenders function. Changed results variable to orders to make more sense. Console logging price per unit to debug NaN error

--- customerOrders.js
+++ customerOrders.js
@@ -1,10 +1,33 @@
-const csv = require('csv-parser');
-const fs = require('fs')
-const results = [];
+const csv = require("csv-parser");
+const fs = require("fs");
+const { parse } = require("path");
+const orders = [];
 
-fs.createReadStream('orders.csv')
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-        console.log(results);
-    })
+fs.createReadStream("orders.csv")
+  .pipe(csv())
+  .on("data", (data) => orders.push(data))
+  .on("end", () => {
+    console.log(orders);
+    findTop5Spenders(orders);
+  });
+
+//Part 1: Total Expenditure Per Customer
+//Return the top 5 customers that spend the most money. Display their customer ID and the Total Spent.
+function findTop5Spenders(orders) {
+  const customerData = {};
+  //console.log(parseFloat(orders[0].price_per_unit));
+  orders.forEach((order) => {
+    let id = order.customer_id;
+    let pricePerUnit = order.pricePerUnit=== ''? 0: parseFloat(order.price_per_unit);
+    let quantity = parseInt(order.quantity);
+    let totalPrice = pricePerUnit * quantity;
+    console.log(pricePerUnit);
+    if (customerData[id]) {
+        customerData[id] += totalPrice;
+    } else {
+        customerData[id] = totalPrice;
+    }
+  });
+  //console.log(customerData);
+}
+
