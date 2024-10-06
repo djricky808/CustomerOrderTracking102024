41e72e8fd95d7ccc72f09d8721864c1cd3597f00
Richard Olpindo <djricky088@yahoo.com>
2024-10-05 14:07:13 -1000

    Created if statement to handle NaN month entries.

---
+++ customerOrder3.js
@@ -0,0 +1,31 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+const { totalmem } = require("os");
+const { parse } = require("path");
+const revenue = [];
+
+fs.createReadStream("orders.csv")
+  .pipe(csv())
+  .on("data", (data) => revenue.push(data))
+  .on("end", () => {
+    //console.log(revenue);
+    monthlyRevenue(revenue);
+  });
+// ## Part 3: Revenue Insights
+
+// 1. **Calculate the total revenue generated for each month**. Your output should display the `year-month` (e.g., `2023-07`) and the corresponding `total_revenue`.
+
+// 2. **Identify any customers who haven't placed an order in the last 6 months** (based on the most recent order date in the dataset). List their `customer_id` and the date of their last order.
+
+function monthlyRevenue(revenue) {
+  const monthlySales = {};
+  revenue.forEach((sale) => {
+    let monthOfSale = sale.order_date.split("-").slice(0, 2);
+    console.log(monthOfSale);
+    if (!isNaN(parseInt(monthOfSale[0]))) {
+      console.log([parseInt(monthOfSale[0]), "This is a year"]);
+    } else {
+      console.log([monthOfSale[0], "This is not a year"]);
+    }
+  });
+}
